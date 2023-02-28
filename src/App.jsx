bc7dd49565cdc363a0868c27e92bec6830059f55
Kevin Ip <ipkevin@gmail.com>
2023-02-27 22:17:53 -0800

    style header for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
+
 import './App.scss';
+import logo from './assets/logo/BrainFlix-logo.svg';
+import mohanPhoto from './assets/images/Mohan-muruge.jpg';
+import uploadIcon from './assets/icons/upload.svg';
 
 function App() {
   return (
     <>
       <header className='header'>
-        <img src='./assets/logo/BrainFlix-logo.svg' alt='Brainflix logo' />
-        <input className="header__search" name="headerSearch" id="headerSearch" placeholder="search"></input>
-        <img src='./assets/images/Mohan-muruge.jpg' alt='user photo' />
-        <button className="header__button button">Upload</button>
+        <div className="header__logo-wrapper"><img className="header__logo" src={logo} alt='Brainflix logo' /></div>
+        <input className="header__search" name="headerSearch" id="headerSearch" placeholder="Search"></input>
+        <img className="header__user-image" src={mohanPhoto} alt='user photo' />
+        <button className="header__button button">Upload
+          <img className="header__button-icon" src={uploadIcon} />
+        </button>
       </header>
+      <section className="video">
+        <video class="video__player">blah</video>
+      </section>
     </>
 
 
