77651626ccd8a18879812ebd6c9d60e143427a6d
Kevin Ip <ipkevin@gmail.com>
2023-02-28 10:01:34 -0800

    add video and description

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,26 @@ function App() {
         </button>
       </header>
       <section className="video">
-        <video class="video__player">blah</video>
+        <video class="video__player" controls poster="https://placekitten.com/1920/1080" src="https://project-2-api.herokuapp.com/stream?api_key=neocat">blah</video>
       </section>
+      <div className="flex-wrapper">
+        <main>
+          <article className="description">
+            <h1 className="description__title">BMX Rampage: 2021 Highlights</h1>
+            <section className="description__credits">
+              <p className="description__author">By Red Crow</p>
+              <p className="description__views">1,001,023</p>
+              <p className="description__date">07/11/2021</p>
+              <p className="description__likes">110,895</p>
+            </section>
+            <p className="description__body">On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title</p>
+          </article>
+
+        </main>
+        <aside>
+
+        </aside>
+      </div>
     </>
 
 
