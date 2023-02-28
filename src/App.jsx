652cccb3c35dd146b0e1cff3943ffae77315ef83
Kevin Ip <ipkevin@gmail.com>
2023-02-27 22:53:47 -0800

    add icons to header search & upload. Remove chrome outline on input fields that have focus. Adjust font sizes in header.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import './App.scss';
 import logo from './assets/logo/BrainFlix-logo.svg';
 import mohanPhoto from './assets/images/Mohan-muruge.jpg';
 import uploadIcon from './assets/icons/upload.svg';
+import searchIcon from './assets/icons/search.svg';
 
 function App() {
   return (
     <>
       <header className='header'>
         <div className="header__logo-wrapper"><img className="header__logo" src={logo} alt='Brainflix logo' /></div>
-        <input className="header__search" name="headerSearch" id="headerSearch" placeholder="Search"></input>
+        <input className="header__search" type="text" name="headerSearch" id="headerSearch" placeholder="Search"></input>
         <img className="header__user-image" src={mohanPhoto} alt='user photo' />
         <button className="header__button button">Upload
           <img className="header__button-icon" src={uploadIcon} />
