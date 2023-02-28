a9d209afec58fd84c07fbf93a30348080e8029ef
Kevin Ip <ipkevin@gmail.com>
2023-02-27 20:49:30 -0800

    add asset folders and assets, add initial header html to app.js as test

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,17 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className='header'>
+        <img src='./assets/logo/BrainFlix-logo.svg' alt='Brainflix logo' />
+        <input className="header__search" name="headerSearch" id="headerSearch" placeholder="search"></input>
+        <img src='./assets/images/Mohan-muruge.jpg' alt='user photo' />
+        <button className="header__button button">Upload</button>
       </header>
-    </div>
+    </>
+
+
+
   );
 }
 
