99378fb86ff19d3b5849c6c74f43db7b8732fcb3
Kevin Ip <ipkevin@gmail.com>
2023-02-28 16:11:48 -0800

    insert and style html description area

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import logo from './assets/logo/BrainFlix-logo.svg';
 import mohanPhoto from './assets/images/Mohan-muruge.jpg';
 import uploadIcon from './assets/icons/upload.svg';
 import searchIcon from './assets/icons/search.svg';
+import likesIcon from './assets/icons/likes.svg';
+import viewsIcon from './assets/icons/views.svg';
 
 function App() {
   return (
@@ -17,7 +19,7 @@ function App() {
         </button>
       </header>
       <section className="video">
-        <video class="video__player" controls poster="https://placekitten.com/1920/1080" src="https://project-2-api.herokuapp.com/stream?api_key=neocat">blah</video>
+        <video className="video__player" controls poster="https://placekitten.com/1920/1080" src="https://project-2-api.herokuapp.com/stream?api_key=neocat">blah</video>
       </section>
       <div className="flex-wrapper">
         <main>
@@ -25,12 +27,16 @@ function App() {
             <h1 className="description__title">BMX Rampage: 2021 Highlights</h1>
             <section className="description__credits">
               <p className="description__author">By Red Crow</p>
-              <p className="description__views">1,001,023</p>
+              <p className="description__views"><img className="description__views-icon" src={viewsIcon} alt="view icon" />1,001,023</p>
               <p className="description__date">07/11/2021</p>
-              <p className="description__likes">110,895</p>
+              <p className="description__likes"><img className="description__likes-icon" src={likesIcon} alt="Likes icon" />110,895</p>
             </section>
             <p className="description__body">On a gusty day in Southern Utah, a group of 25 daring mountain bikers blew the doors off what is possible on two wheels, unleashing some of the biggest moments the sport has ever seen. While mother nature only allowed for one full run before the conditions made it impossible to ride, that was all that was needed for event veteran Kyle Strait, who won the event for the second time -- eight years after his first Red Cow Rampage title</p>
           </article>
+
+          <section className="comments">
+            
+          </section>
 
         </main>
         <aside>
