e55ca652a1800cbaf96c7cac5571637fbd757d33
Kevin Ip <ipkevin@gmail.com>
2023-02-28 16:34:13 -0800

    add initial comments html

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,56 @@ function App() {
           </article>
 
           <section className="comments">
-            
+            <p className="comments__count">3 comments</p>
+            <div className="comments__wrapper">
+              <img className="comments__image" alt="profile photo" src={mohanPhoto} />
+              <div className="comments__content">
+                <h3 className="comments__form-subtitle">Join the conversation</h3>
+                <form className="comments__form-wrapper">
+                  <textarea className="comments__text-field" placeholder="Add a new comment"></textarea>
+                  <button className="comments__button">Comment</button>
+                </form>
+              </div>
+            </div>
+            <ul className="comments__list">
+              <li className="comments__wrapper">
+                <img className="comments__image" alt="profile photo" src="" />
+                <div className="comments__content">
+                  <div className="comments__meta">
+                    <p className="comments__author">Michael Lyons</p>
+                    <p className="comments__date">11/10/2023</p>
+                  </div>
+                  <p className="comments__text">
+                    They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.
+                  </p>
+                </div>
+              </li>
+              <li className="comments__wrapper">
+                <img className="comments__image" alt="profile photo" src="" />
+                <div className="comments__content">
+                  <div className="comments__meta">
+                    <p className="comments__author">Michael Lyons</p>
+                    <p className="comments__date">11/10/2023</p>
+                  </div>
+                  <p className="comments__text">
+                    They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.
+                  </p>
+                </div>
+              </li>
+              <li className="comments__wrapper">
+                <img className="comments__image" alt="profile photo" src="" />
+                <div className="comments__content">
+                  <div className="comments__meta">
+                    <p className="comments__author">Michael Lyons</p>
+                    <p className="comments__date">11/10/2023</p>
+                  </div>
+                  <p className="comments__text">
+                    They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.
+                  </p>
+                </div>
+              </li>
+
+            </ul>
           </section>
 
         </main>
