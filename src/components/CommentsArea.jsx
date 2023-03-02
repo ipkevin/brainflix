c360aa7578c32351ee6011e2b9741b42502423b9
Kevin Ip <ipkevin@gmail.com>
2023-03-02 14:36:56 -0800

    prettyfying code

--- src/components/CommentsArea.jsx
+++ src/components/CommentsArea.jsx
@@ -1,7 +1,7 @@
-import Comment from './Comment';
-import mohanPhoto from '../assets/images/Mohan-muruge.jpg';
+import Comment from "./Comment";
+import mohanPhoto from "../assets/images/Mohan-muruge.jpg";
 
-import './CommentsArea.scss';
+import "./CommentsArea.scss";
 
 // expects props to have a selectedVideo object in it
 export default function CommentsArea(props) {
@@ -9,22 +9,20 @@ export default function CommentsArea(props) {
         <section className="comments">
             <p className="comments__count">{props.selectedVideo.comments.length} comments</p>
             <div className="comments__wrapper">
-              <img className="comments__image comments__image--form" alt="profile photo" src={mohanPhoto} />
-              <div className="comments__content">
-                <p className="comments__form-subtitle">Join the conversation</p>
-                <form className="comments__form-wrapper" onSubmit={(event)=>event.preventDefault()}>
-                  <textarea className="comments__text-field" placeholder="Add a new comment"></textarea>
-                  <button className="comments__button button">Comment</button>
-                </form>
-              </div>
+                <img className="comments__image comments__image--form" alt="profile photo" src={mohanPhoto} />
+                <div className="comments__content">
+                    <p className="comments__form-subtitle">Join the conversation</p>
+                    <form className="comments__form-wrapper" onSubmit={(event) => event.preventDefault()}>
+                        <textarea className="comments__text-field" placeholder="Add a new comment"></textarea>
+                        <button className="comments__button button">Comment</button>
+                    </form>
+                </div>
             </div>
             <ul className="comments__list">
-
-              {props.selectedVideo.comments.map( element => {
-                return <Comment commentData={element} key={element.id} />
-              })}
-
+                {props.selectedVideo.comments.map((element) => {
+                    return <Comment commentData={element} key={element.id} />;
+                })}
             </ul>
         </section>
-    )   
+    );
 }
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,26 +1,23 @@
+import Video from "./Video";
 
-import Video from './Video';
-
-import './VideoList.scss';
+import "./VideoList.scss";
 
 // Expecting that props has a videoListArr array that contains all of the video objects with minimal details
 export default function VideoList(props) {
-    
     // filter the video array to remove the currently selected video
     let currID = props.selectedVideo.id;
-    const filteredArr = props.videoListArr.filter( element => {
-        return (element.id != currID);
-    })
-    
+    const filteredArr = props.videoListArr.filter((element) => {
+        return element.id != currID;
+    });
+
     return (
         <aside className="videoList">
             <p className="videoList__subtitle">Next Videos</p>
             <ul className="videoList__wrapper">
-                {filteredArr.map (videoItem => (
-                    <Video videoInfo={videoItem} key={videoItem.id} videoListClickHandler={props.videoListClickHandler} />    
-                    )
-                )}
+                {filteredArr.map((videoItem) => (
+                    <Video videoInfo={videoItem} key={videoItem.id} videoListClickHandler={props.videoListClickHandler} />
+                ))}
             </ul>
         </aside>
-    )
+    );
 }
