5b7649861a7b93d97d7edd7d36fe48cd1a462f4d
Kevin Ip <ipkevin@gmail.com>
2023-03-01 17:30:57 -0800

    update videoplayer and meta to use data from json using state and props

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,9 +1,9 @@
 import './VideoPlayer.scss';
 
-export default function VideoPlayer() {
+export default function VideoPlayer(props) {
     return (
       <section className="video">
-        <video className="video__player" controls poster="https://placekitten.com/1920/1080" src="https://project-2-api.herokuapp.com/stream?api_key=neocat">blah</video>
+        <video className="video__player" controls poster={props.selectedVideo.image} src={props.selectedVideo.video}>blah</video>
       </section>
     )
 }
