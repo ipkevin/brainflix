6a12af3cba190560cfd69717b3be9ccbd853292d
Kevin Ip <ipkevin@gmail.com>
2023-03-01 21:13:54 -0800

    update videolist so that it will not show the currently selected video

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -5,11 +5,18 @@ import './VideoList.scss';
 
 // Expecting that props has a videoListArr array that contains all of the video objects with minimal details
 export default function VideoList(props) {
+    
+    // filter the video array to remove the currently selected video
+    let currID = props.selectedVideo.id;
+    const filteredArr = props.videoListArr.filter( element => {
+        return (element.id != currID);
+    })
+    
     return (
         <aside className="videoList">
             <p className="videoList__subtitle">Next Videos</p>
             <ul className="videoList__wrapper">
-                {props.videoListArr.map (videoItem => (
+                {filteredArr.map (videoItem => (
                     <Video videoInfo={videoItem} key={videoItem.id} videoListClickHandler={props.videoListClickHandler} />    
                     )
                 )}
