1c819a3109d34216384f3d7fcfa8cc7c3759da55
Kevin Ip <ipkevin@gmail.com>
2023-03-02 14:52:47 -0800

    finish styling for mobile view, add font family value for placeholder text in textarea of comments form, prettify more components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
-import logo from '../assets/logo/BrainFlix-logo.svg';
-import mohanPhoto from '../assets/images/Mohan-muruge.jpg';
-import uploadIcon from '../assets/icons/upload.svg';
+import logo from "../assets/logo/BrainFlix-logo.svg";
+import mohanPhoto from "../assets/images/Mohan-muruge.jpg";
+import uploadIcon from "../assets/icons/upload.svg";
 
-import './Header.scss';
+import "./Header.scss";
 
 export default function Header() {
     return (
-        <header className='header'>
-        <div className="header__logo-wrapper"><img className="header__logo" src={logo} alt='Brainflix logo' /></div>
-        <input className="header__search" type="text" name="headerSearch" id="headerSearch" placeholder="Search"></input>
-        <img className="header__user-image" src={mohanPhoto} alt='user photo' />
-        <button className="header__button button">Upload
-        <img className="header__button-icon" src={uploadIcon} />
-        </button>
+        <header className="header">
+            <div className="header__logo-wrapper">
+                <img className="header__logo" src={logo} alt="Brainflix logo" />
+            </div>
+            <input className="header__search" type="text" name="headerSearch" id="headerSearch" placeholder="Search"></input>
+            <img className="header__user-image" src={mohanPhoto} alt="user photo" />
+            <button className="header__button button">
+                Upload
+                <img className="header__button-icon" src={uploadIcon} />
+            </button>
         </header>
-    )
+    );
 }
