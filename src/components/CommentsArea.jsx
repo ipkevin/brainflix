bdd8cc29f901ff61d6ca58eb7aea0c2adaef1595
Kevin Ip <ipkevin@gmail.com>
2023-03-02 16:20:44 -0800

    add tablet styling for whole page

--- src/components/CommentsArea.jsx
+++ src/components/CommentsArea.jsx
@@ -8,7 +8,7 @@ export default function CommentsArea(props) {
     return (
         <section className="comments">
             <p className="comments__count">{props.selectedVideo.comments.length} comments</p>
-            <div className="comments__wrapper">
+            <div className="comments__wrapper comments__wrapper-form">
                 <img className="comments__image comments__image--form" alt="profile photo" src={mohanPhoto} />
                 <div className="comments__content">
                     <p className="comments__form-subtitle">Join the conversation</p>
