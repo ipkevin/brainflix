84d8a8e5e4e264df93b0b90857d48c0d37d83b52
Kevin Ip <ipkevin@gmail.com>
2023-03-03 15:24:51 -0800

    add data rename functionality for dive deeper

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,6 +1,28 @@
 import blankPhoto from "../assets/images/profile-blank.png";
 import "./Comment.scss";
 
+// Converts timestamp to a "x sec/min/hour/days ago" string & returns it
+const convertDate = function(dateIn){
+    let currDate = new Date();
+    let commDate = new Date(dateIn);
+    const elapsed = currDate - commDate;
+    console.log(elapsed);
+    const oneSec = 1000;
+    const oneMin = 60*oneSec;
+    const oneHour = 60*oneMin;
+    const oneDay = 24*oneHour;
+
+    if (elapsed < oneMin) {
+        return `${Math.floor(elapsed/oneSec)} seconds ago`;
+    } else if (elapsed < oneHour) {
+        return `${Math.floor(elapsed/oneMin)} minutes ago`;
+    } else if (elapsed < oneDay) {
+        return `${Math.floor(elapsed/oneHour)} hours ago`;
+    } else {
+        return `${Math.floor(elapsed/oneDay)} days ago`;
+    }
+}
+
 export default function Comment(props) {
     return (
         <li className="comments__wrapper">
@@ -8,7 +30,8 @@ export default function Comment(props) {
         <div className="comments__content">
         <div className="comments__meta">
             <p className="comments__author">{props.commentData.name}</p>
-            <p className="comments__date">{new Date(props.commentData.timestamp).toLocaleDateString()}</p>
+            {/* <p className="comments__date">{new Date(props.commentData.timestamp).toLocaleDateString()}</p> */}
+            <p className="comments__date">{convertDate(props.commentData.timestamp)}</p>
         </div>
         <p className="comments__text">{props.commentData.comment}</p>
         </div>
