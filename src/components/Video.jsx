9d43ca19a17a9e5f636bc794967adc64d336a8d7
Kevin Ip <ipkevin@gmail.com>
2023-03-05 16:44:34 -0800

    adjust styling to better match mockups

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -6,11 +6,13 @@ export default function Video(props) {
 
     // Crops the video title if screen size is smaller than 768px wide. Crops at 40 characters and does not crop
     // in middle of words.
+    // Using fxn instead of CSS-only solution as all fully supported CSS solutions limit based on width of container not chars
     function cropMobile(){
         let fullTitle = props.videoInfo.title;
         if ((fullTitle.length > 40) && (window.innerWidth < 768)) {
             // If last word would be truncated by char limit, then truncate before it.
-            let lastSpace = fullTitle.lastIndexOf(" ",40); // use 1 char longer to find blank in case last word just fits inside limit.
+            // Using 1 char longer to find blank allows for case where last word just fits inside limit.
+            let lastSpace = fullTitle.lastIndexOf(" ",40); 
             return ((fullTitle.slice(0,lastSpace)).trim() + "...");
         } else {
             return fullTitle;
