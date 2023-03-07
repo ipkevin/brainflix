0fe8dd48dca03ceeb40e4709404c2e8d4fcdea60
Kevin Ip <ipkevin@gmail.com>
2023-03-06 16:26:48 -0800

    make logo linkable and fix styling, convert header upload to link instead of button, move state and handler function into homepage component for now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import {Link} from "react-router-dom";
 import logo from "../assets/logo/BrainFlix-logo.svg";
 import mohanPhoto from "../assets/images/Mohan-muruge.jpg";
 
@@ -7,11 +8,13 @@ export default function Header() {
     return (
         <header className="header">
             <div className="header__logo-wrapper">
-                <img className="header__logo" src={logo} alt="Brainflix logo" />
+                <Link className="header__logo-link" to="/">
+                    <img className="header__logo" src={logo} alt="Brainflix logo" />
+                </Link>
             </div>
             <input className="header__search" type="text" name="headerSearch" id="headerSearch" placeholder="Search"></input>
             <img className="header__user-image" src={mohanPhoto} alt="user photo" />
-            <button className="header__button button">Upload</button>
+            <Link to="/upload" className="header__button button">Upload</Link>
         </header>
     );
 }
